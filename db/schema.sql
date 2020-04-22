cb1d661643c6bb7393f2cee23d6510f720130c9b
Kasey Chang <kschang77@yahoo.com>
2020-04-22 08:37:45 -0700

    20200422 KC Testing Deployment changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,9 @@
-###
-Schema
-
-CREATE DATABASE klingon_db;
-USE klingon_db;
 
 CREATE TABLE klingons
 (
-	id int NOT NULL
-	AUTO_INCREMENT,
-	kword varchar
-	(255) NOT NULL,
-	english varchar
-	(255) NOT NULL,
-    mastered BOOLEAN DEFAULT false,
-	PRIMARY KEY
-	(id)
+	id int NOT NULL	AUTO_INCREMENT,
+	kword varchar(255) NOT NULL,
+	english varchar(255) NOT NULL,
+   	mastered BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
