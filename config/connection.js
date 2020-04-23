c2977f453b857ea981492108b5fd21ca889282a8
Kasey Chang <kschang77@yahoo.com>
2020-04-22 18:13:07 -0700

    20200422 KC Fixed sql Pool

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,13 @@
 var mysql = require("mysql");
 
 var mysqlPool = mysql.createPool({
-
+  connectionLimit: 10,
   user: "bdfb93be299cc3",
   password: "ca214447",
   host: "us-cdbr-iron-east-01.cleardb.net",
   database: "heroku_a9783d6bd1fc647"
 
+  // connectionLimit: 10,
   // host: "localhost",
   // port: 3306,
   // user: "root",
@@ -16,21 +17,24 @@ var mysqlPool = mysql.createPool({
 });
 
 // changing to cloud mySQL...
-
 // user: "bdfb93be299cc3"
 // password: "ca214447"
 // host: "us-cdbr-iron-east-01.cleardb.net"
 // database: "heroku_a9783d6bd1fc647"
 
-
 // Make connection.
 mysqlPool.getConnection(function (err, connection) {
   if (err) {
+    res.json({ "code": 100, "status": "Error connected to database " + err.stack })
     console.error("error connecting: " + err.stack);
-    return;
   }
-  console.log("connected as id " + connection.threadId);
+  if (connection) connection.release();
+  return;
 });
 
+// console.log("connected as id " + connection.threadId);
+// console.log("connection " + connection);
+module.exports = mysqlPool;
+
+
 // Export connection for our ORM to use.
-module.exports = connection;
